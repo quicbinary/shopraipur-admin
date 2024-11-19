8f0b0da1e5fe1a60eee58dcae02df23b4e70e6a4
Lostanurag <anuragsolanki299@gmail.com>
2024-11-19 23:19:42 +0530

    pushed shops filter

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     images: {
-      domains: ['picsum.photos', 'via.placeholder.com'], // Add the external image domain
+      domains: ['picsum.photos', 'via.placeholder.com', 'img.freepik.com'], // Add the external image domain
     },
     experimental: {
         appDir: true, // Ensure that the app directory is enabled
