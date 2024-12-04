97cdc4c4601d54633ef71e9750096fab484ca0cb
Aasvanbhhayre <aasvanbhayreaasvan@gmail.com>
2024-12-04 17:08:23 +0530

    sponsorship

--- src/app/ads/page.js
+++ src/app/ads/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Selectaddtype from '@/components/ads/selectaddtype'
+import Sponsorship from "@/components/ads/sponsorship"
 export default function page() {
   return (
     <div>
-    
+    <Sponsorship/>
     <Selectaddtype/>
     </div>
   )
